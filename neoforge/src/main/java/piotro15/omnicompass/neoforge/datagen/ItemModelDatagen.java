50cc30e17d74c1ae293fe29352d59a0de45ca85b
PiotrO15 <72670117+PiotrO15@users.noreply.github.com>
2026-09-02 21:34:02 +0200

    Fixed wrong compass needle rotation, bump version

--- neoforge/src/main/java/piotro15/omnicompass/neoforge/datagen/ItemModelDatagen.java
+++ neoforge/src/main/java/piotro15/omnicompass/neoforge/datagen/ItemModelDatagen.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.item.RangeSelectItemModel;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.Mth;
 import org.jspecify.annotations.NonNull;
 import piotro15.omnicompass.OmniCompass;
 import piotro15.omnicompass.client.CompassNeedleTintSource;
@@ -20,6 +21,7 @@ import piotro15.omnicompass.common.registry.ModItems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 
@@ -66,12 +68,14 @@ public class ItemModelDatagen extends ModelProvider {
     private List<RangeSelectItemModel.Entry> createNeedleModels(BiConsumer<Identifier, ModelInstance> modelOutput) {
         List<RangeSelectItemModel.Entry> overrides = new ArrayList<>();
 
-        ItemModel.Unbaked base = ItemModelUtils.tintedModel(ModelTemplates.TWO_LAYERED_ITEM.create(OmniCompass.id("compass/needle_00"), TextureMapping.layered(new Material(OmniCompass.id("item/shade_00")), new Material(OmniCompass.id("item/needle_00"))), modelOutput), new CompassNeedleTintSource(true), new CompassNeedleTintSource(false));
+        ItemModel.Unbaked base = ItemModelUtils.tintedModel(ModelTemplates.TWO_LAYERED_ITEM.create(OmniCompass.id("compass/needle_16"), TextureMapping.layered(new Material(OmniCompass.id("item/shade_16")), new Material(OmniCompass.id("item/needle_16"))), modelOutput), new CompassNeedleTintSource(true), new CompassNeedleTintSource(false));
         overrides.add(ItemModelUtils.override(base, 0.0F));
 
         for (int i = 1; i < 32; i++) {
-            Identifier id = OmniCompass.id("compass/needle_" + (i < 10 ? "0" + i : i));
-            ItemModel.Unbaked overrideModel = ItemModelUtils.tintedModel(ModelTemplates.TWO_LAYERED_ITEM.create(id, TextureMapping.layered(new Material(OmniCompass.id("item/shade_" + (i < 10 ? "0" + i : i))), new Material(OmniCompass.id("item/needle_" + (i < 10 ? "0" + i : i)))), modelOutput), new CompassNeedleTintSource(true), new CompassNeedleTintSource(false));
+            String textureIndex = String.format(Locale.ROOT, "%02d", Mth.positiveModulo(i - 16, 32));
+
+            Identifier id = OmniCompass.id("compass/needle_" + textureIndex);
+            ItemModel.Unbaked overrideModel = ItemModelUtils.tintedModel(ModelTemplates.TWO_LAYERED_ITEM.create(id, TextureMapping.layered(new Material(OmniCompass.id("item/shade_" + textureIndex)), new Material(OmniCompass.id("item/needle_" + textureIndex))), modelOutput), new CompassNeedleTintSource(true), new CompassNeedleTintSource(false));
 
             overrides.add(ItemModelUtils.override(overrideModel, i - 0.5F));
         }
